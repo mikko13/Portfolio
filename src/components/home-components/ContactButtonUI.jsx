644ff742cd96b3292d63e2d0d98f44e0b695ac38
mikko13 <mikko.samaniego.cics@ust.edu.ph>
2024-11-14 02:13:39 +0800

    Update ContactButtonUI.jsx

--- src/components/home-components/ContactButtonUI.jsx
+++ src/components/home-components/ContactButtonUI.jsx
@@ -4,18 +4,18 @@ const ContactButtonUI = ({ isVisible }) => {
   return (
     <div
       id="contact-button"
-      className={`flex justify-center md:justify-start w-full transition-transform duration-700 ease-out ${
+      className={`flex justify-center md:justify-start w-full transition-transform duration-300 ease-out ${
         isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-5'
       }`}
     >
-      <button className="relative flex items-center px-6 py-3 overflow-hidden font-medium transition-all bg-[#1C3C4D] rounded-md group">
-        <span className="absolute top-0 right-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-[#3e6579] rounded group-hover:-mr-4 group-hover:-mt-4">
+      <button className="relative flex items-center px-6 py-3 overflow-hidden font-medium bg-[#1C3C4D] rounded-md group">
+        <span className="absolute top-0 right-0 inline-block w-4 h-4 duration-500 ease-in-out bg-[#3e6579] rounded group-hover:-mr-4 group-hover:-mt-4">
           <span className="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white"></span>
         </span>
-        <span className="absolute bottom-0 rotate-180 left-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-[#3e6579] rounded group-hover:-ml-4 group-hover:-mb-4">
+        <span className="absolute bottom-0 rotate-180 left-0 inline-block w-4 h-4 duration-500 ease-in-out bg-[#3e6579] rounded group-hover:-ml-4 group-hover:-mb-4">
           <span className="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white"></span>
         </span>
-        <span className="absolute bottom-0 left-0 w-full h-full transition-all duration-500 ease-in-out delay-200 -translate-x-full bg-[#3e6579] rounded-md group-hover:translate-x-0"></span>
+        <span className="absolute bottom-0 left-0 w-full h-full duration-500 ease-in-out delay-200 -translate-x-full bg-[#3e6579] rounded-md group-hover:translate-x-0"></span>
         <span className="relative w-full text-left text-white transition-colors duration-200 ease-in-out group-hover:text-white">
           Contact Now!
         </span>
